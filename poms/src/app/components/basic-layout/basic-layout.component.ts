b6e8a13d19622bc8f635e47ffc141728d253bad9
SirBenedick <beneweb@outlook.de>
2019-05-15 18:32:50 +0200

    created right sidebar

--- poms/src/app/components/basic-layout/basic-layout.component.ts
+++ poms/src/app/components/basic-layout/basic-layout.component.ts
@@ -1,4 +1,9 @@
+import { Subscription } from 'rxjs';
+import { Observable, timer } from 'rxjs';
+import { BackendService } from './../../services/backend.service';
 import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { IPrinterData } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-basic-layout',
@@ -7,13 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicLayoutComponent implements OnInit {
 
-  constructor() { }
+  printerSubscription: Observable<Object>;
 
+  constructor(private backendService: BackendService) { 
+  }
+  
   ngOnInit() {
-    
+    console.log("basic ngOnInit")
+    this.printerSubscription = this.backendService.allPrinterDataObservable
+    // .subscribe((newPrinterData: Array<IPrinterData>) => {
+    //   console.log("Polling new Data", newPrinterData);
+    //   // this.allPrinterData = newPrinterData;
+    // });
+
   }
 
   newOrder(){
     console.log("function called");
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    
   }
 }
