cc75069333f19021ef5e273b9af7c74ecc4318ae
SirBenedick <beneweb@outlook.de>
2019-05-31 11:50:34 +0200

    small text fixes, startet new Printer Popup

--- poms/src/app/components/pop-up-neuer-drucker/pop-up-neuer-drucker.component.ts
+++ poms/src/app/components/pop-up-neuer-drucker/pop-up-neuer-drucker.component.ts
@@ -1,34 +1,39 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { IPrinterData } from 'src/app/shared/interfaces';
-import { PrinterComponent } from 'src/app/pages/printer/printer.component';
+import { Component, OnInit, Inject } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { IPrinterData } from "src/app/shared/interfaces";
+import { PrinterComponent } from "src/app/pages/printer/printer.component";
 
 @Component({
-  selector: 'app-pop-up-neuer-drucker',
-  templateUrl: './pop-up-neuer-drucker.component.html',
-  styleUrls: ['./pop-up-neuer-drucker.component.css']
+  selector: "app-pop-up-neuer-drucker",
+  templateUrl: "./pop-up-neuer-drucker.component.html",
+  styleUrls: ["./pop-up-neuer-drucker.component.css"]
 })
 export class PopUpNeuerDruckerComponent implements OnInit {
   newPrinterForm: FormGroup;
-  constructor(  
+  constructor(
     @Inject(MAT_DIALOG_DATA) public data: IPrinterData,
-    public dialogRef: MatDialogRef<PrinterComponent>) { }
+    public dialogRef: MatDialogRef<PrinterComponent>
+  ) {}
 
   ngOnInit() {
-     this.newPrinterForm = new FormGroup({
-       printerId: new FormControl(this.data.printer_id ? this.data.printer_id : "", 
-   [Validators.minLength(1),Validators.required ]
-     ),
-       printerName: new FormControl(this.data.name ? this.data.name : "", [
-         Validators.minLength(1),
-         Validators.required
-       ]),
-     });
+    this.newPrinterForm = new FormGroup({
+      printerName: new FormControl("", [
+        Validators.minLength(1),
+        Validators.required
+      ]),
+      host: new FormControl("", [
+        Validators.minLength(1),
+        Validators.required
+      ]),
+      port: new FormControl("", [
+        Validators.minLength(1),
+        Validators.required
+      ])
+    });
   }
 
-
-  onBreakButton():void{
+  onBreakButton(): void {
     this.dialogRef.close();
   }
 }
