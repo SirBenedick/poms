3233715e5807fb5ebac5c7aed91e5e2c222bffa6
Maxi <MaxiSchmitt@t-online.de>
2019-06-14 14:41:28 +0200

    Nachbereitung angepasst wie Auftragsübersicht

--- src/app/pages/printedorders/printedorders.component.ts
+++ src/app/pages/printedorders/printedorders.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import {
   IOrder,
   IFilterOrders,
-  IGroupedOrders
+  IGroupedOrders,
+  IResinType
 } from "src/app/shared/interfaces";
 import { BackendService } from "src/app/services/backend.service";
 import { MatDialog } from "@angular/material";
@@ -197,5 +198,10 @@ export class PrintedordersComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) this.filterGroupData(result.data);
     });
+  }
+  getResineColorValue(resine_name: string) {
+    return this.backendService.resineData.find(
+      (harz: IResinType) => harz.resin_name == resine_name
+    ).color;
   }
 }
